dce937bc3008ae020b944be8b75462023011b684
antonchik <ganseanderson@gmail.com>
2022-11-28 22:04:05 +0300

    les1 SOAP. Приложения сервера и клиента

---
+++ LibraryService_/Services/Impl/LibraryDatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using LibraryService_.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LibraryService_.Services.Impl
+{
+    public class LibraryDatabaseContext : ILibraryDatabaseContextService
+    {
+        private IList<Book> _libraryDatabase;
+
+        public IList<Book> Books => _libraryDatabase;
+
+
+        public LibraryDatabaseContext()
+        {
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            _libraryDatabase =
+                 (List<Book>)JsonConvert.DeserializeObject(Encoding.UTF8.GetString(Properties.Resources.books), typeof(List<Book>));//берем из ресурсов приложения- в свойстве сервиса можно в разделе "Ресурсы" задать источник данных-добавляем файл с данными
+        }
+
+    }
+}
